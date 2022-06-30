6d51978631ca315d668ea5f9a1bd87261409c8b0
Hong Zhang <sheetstone@gmail.com>
2022-06-30 17:19:07 -0500

    update

---
+++ htdocs/newProduct/index.php
@@ -0,0 +1,234 @@
+<?php
+include_once('../../includes/inc.php');
+htmlHeader(array("title"=>"","metaDesc"=>"Dedicated to improving cancer outcomes via liquid biopsies.","preLoader"=>"0"));
+?>
+<div class="main-container">
+    <section class="image-bg overlay parallax pt40 pb40 pt-xs-80 pb-xs-80">
+        <div class="background-image-holder">
+            <img alt="image" class="background-image overlay" src="/img/lungTumor.jpg">
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <h1>Title</h1>
+            </div>
+
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-md-4">
+                    <h2 class="color-primary">Something</h2>
+                    <hr>
+                    <table class="table table-striped mutTable">
+                        <thead>
+                            <tr>
+                                <th>SNV/Indel</th>
+                                <th>Fusions</th>
+                                <th>CNV</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="gene">AKT1<br>ALK<br>B2M<br>BRAF<br>EGFR<br>ERBB2
+                                    (HER2)<br>FGFR2<br>FGFR3<br>KEAP1<br>KRAS<br>MAP2K1
+                                    (MEK1)<br>MET<br>NRAS<br>PIK3CA<br>RET<br>ROS1<br>STK11</td>
+                                <td class="gene">ALK<br>EGFR<br>FGFR2<br>FGFR3<br>NTRK1<br>RET<br>ROS1</td>
+                                <td class="gene">B2M<br>EGFR<br>ERBB2
+                                    (HER2)<br>FGFR1</br>KRAS<br>MET<br>MYC<br>NTRK1<br>PIK3CA<br>PTEN<br>RICTOR<br>STK11
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-sm-6 col-md-6 col-md-offset-1">
+                    <h2 class="color-primary">Something 2</h2>
+                    <hr>
+                    <table class="table table-striped mutTable">
+                        <thead>
+                            <tr>
+                                <th>Biomarker</th>
+                                <th>Therapy</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>exon 19 deletion<br>exon 21 L858R</td>
+                                <td>Gilotrif® (afatinib)<br>Tarceva&reg; (erlotinib)<br>Vizimpro&reg;
+                                    (dacomitinib)<br>Iressa&reg; (gefitinib)<br>Tagrisso&reg; (osimertinib)</td>
+                            </tr>
+                            <tr>
+                                <td>EGFR S768I, L861Q, and/or G719X</td>
+                                <td>Gilotrif&reg; (afatinib)</td>
+                            </tr>
+                            <tr>
+                                <td>ALK fusions</td>
+                                <td>Alecensa&reg; (alectinib)<br>Alunbrig&reg; (brigatinib)<br>Lorbrena&reg;
+                                    (lorlatinib)<br>Xalkori&reg; (crizotinib)<br>Zykadia&reg;(ceritinib)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>ROS1 fusions</td>
+                                <td>Xalkori&reg; (crizotinib)<br>Zykadia&reg;(ceritinib)<br>Rozlytrek&reg; (entrectinib)
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>NTRK1 fusions</td>
+                                <td>Vitrakvi&reg; (larotrectinib)<br>Rozlytrek&reg; (entrectinib)</td>
+                            </tr>
+                            <tr>
+                                <td>BRAF V600E</td>
+                                <td>Tafinlar&reg; (dabrafenib) in combination with Mekinist® (trametinib)</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="page-title page-title-3 image-bg overlay parallax">
+        <div class="background-image-holder">
+            <img alt="Background Image" class="background-image" src="/img/bloodcells.jpg">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h3 class="uppercase mb0">Something 3</h3>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row mb0 mb-xs-24">
+                <div class="col-sm-6">
+                    <h2 class="color-primary">Clinical Utility</h2>
+                    <p>She sat in the darkened room waiting. It was now a standoff. He had the power to put her in the
+                        room, but not the power to make her repent. It wasn't fair and no matter how long she had to
+                        endure the darkness, she wouldn't change her attitude. At three years old, Sandy's stubborn
+                        personality had already bloomed into full view.</p>
+                    <p>"Do Not Enter." The sign made it clear that they didn't want anyone around. That wasn't going to
+                        stop Jack. Jack always lived with the notion that signs were mere suggestions, not actually
+                        absolute rules. That's why the moment Jack looked at the "Do Not Enter" sign, he walked past it
+                        and onto their property.</p>
+                    <ul>
+                        <li>chapter</li>
+                        <li>honey</li>
+                        <li>homework</li>
+                        <li>potato</li>
+                        <li>marketing</li>
+                        <li>topic</li>
+                        <li>reception</li>
+                        <li>basket</li>
+                        <li>indication</li>
+                        <li>engine</li>
+                        <li>judgment</li>
+                        <li>feedback</li>
+                        <li>significance</li>
+                    </ul>
+                </div>
+                <div class="col-sm-offset-6 text-center"><a href="https://doi.org/10.1093/jnci/djy156"
+                        target="_new"><img alt="Screenshot" src="/img/msk-JNCI.png"></a><br>
+                    <img alt="Article Image" class="mb32 mb-xs-16" src="/img/tat.png" style="width:50%">
+                </div>
+            </div>
+            <div class="col-sm-12">
+                <p>Below are the spectrum and number of SNVs, Indels, CNV, and fusions detected by the ctDx Lung assay
+                    in the first 210 NSCLC patients.</p>
+            </div>
+        </div>
+        <div class="row mb8 mb-xs-24">
+            <div id="MSKContainer" width="100%" style="margin-top: 0px;">
+                <div id="dataWindow"></div>
+            </div>
+    </section>
+
+    <section class="page-title page-title-3 image-bg overlay parallax">
+        <div class="background-image-holder">
+            <img alt="Background Image" class="background-image" src="/img/squareFusion.jpg"
+                style="background-size: 800px 533px;">
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                    <h3 class="uppercase mb0">Fusions</h3>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row mb8 mb-xs-24">
+                <div class="col-sm-8 col-md-8 col-md-offset-1">
+                    <h2 class="color-primary">Fusion Detection</h2>
+                    <p>Researchers at the <b>Vanderbilt University Medical Center</b> used the Resolution ctDx Lung
+                        assay in the study &quot;<a href="https://doi.org/10.1016/j.jtho.2019.08.003"
+                            target="_new">Monitoring Therapeutic Response and Resistance: Analysis of Circulating Tumor
+                            DNA in Patients With ALK+ Lung Cancer</a>&quot; (<a
+                            href="https://doi.org/10.1016/j.jtho.2019.08.003"
+                            target="_new">doi.org/10.1016/j.jtho.2019.08.003</a>) to detect ALK gene fusions. The
+                        detected EML4-ALK rearrangments are plotted on the interactive graph below. The ctDx platform
+                        detects gene fusions / rearrangements without <i>a prior</i> knowledge of breakpoint or gene
+                        partner, resulting in the ability to not only capture canonical fusions, but also discover novel
+                        fusion partners.
+                    <p>
+                </div>
+                <div class="col-md-3">
+                    <a href="https://doi.org/10.1016/j.jtho.2019.08.003" target="_new"><img alt="Screenshot"
+                            src="/img/VCC_IASLC.png"></a>
+                </div>
+                <div class="row mb8 mb-xs-24">
+                    <div id="fusionContainer" width="100%" style="margin-top: 0px;"></div>
+                </div>
+                <div class="row mb32 mb-xs-24">
+                    <h6>
+                        <center>Pan by dragging along the genomic coordinates. Zoom by double clicking or scroll wheel
+                            over the genomic coordinates.</center>
+                    </h6>
+                </div>
+            </div>
+            <div class="row mb8 mb-xs-24">
+                <div class="col-sm-8 col-md-8 col-md-offset-1">
+                    <p>The <b>Dana-Farber Cancer Institute</b> compared the Resolution ctDx Lung Assay against Guardant
+                        Health's Guardant360&reg; assay in the detection of the more challenging oncogenic gene fusions.
+                        The resulting paper, <i><a href="https://doi.org/10.1016/j.lungcan.2019.06.004"
+                                target="_new">Sensitivity of next-generation sequencing assays detecting oncogenic
+                                fusions T in plasma cell-free DNA</a></i> (<a
+                            href="https://doi.org/10.1016/j.lungcan.2019.06.004"
+                            target="_new">doi.org/10.1016/j.lungcan.2019.06.004</a>) </p>
+                    <p><i>Results: </i>Of 16 patients assayed known to harbor an ALK, ROS1, or RET in tumor, G360
+                        detected fusions in 7 cases, Resolution ctDx Lung detected fusions in 13 cases, and 3 cases were
+                        detected by neither. Of the 7 fusions detected by both assays, G360 reported lower mutant
+                        allelic fractions (AF). In cases missed by G360, tumor derived TP53 mutations were often
+                        detected confirming presence of tumor DNA. Raw sequencing data showed that inverted or
+                        out-of-frame variants were overrepresented in cases detected using ctDx Lung but not by G360.
+                    </p>
+
+                </div>
+                <div class="col-md-3">
+                    <a href="https://doi.org/10.1016/j.lungcan.2019.06.004" target="_new"><img alt="Screenshot"
+                            src="/img/DFCI_Lung_Cancer.png"></a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-11 col-md-11 col-md-offset-1 text-center">
+                    <img alt="Article Image" class="mb0" src="/img/Supplee_et_al_fusions.png" style="width:95%"><br>
+                    <small class="text-muted">Supplee et al. <i>Lung Cancer</i> 2019;134:96-99</small>
+                </div>
+            </div>
+    </section>
+</div>
+</div>
+
+</div>
+
+<?php
+footer('
+<script src="https://d3js.org/d3.v4.min.js" type="text/javascript"></script>
+<script src="/js/bootstrap.popover.js"></script>
+<script src="/js/msk.ob.js"></script>
+<script src="/js/fusion_jto.ob.js"></script>
+');
